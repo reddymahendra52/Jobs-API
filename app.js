9281314df1978429b690b0144cb51761ff3a7523
Mutukundu Mahendra Reddy <reddymahendra52@gmail.com>
2023-04-04 13:51:42 +0530

    swagger docs added

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
 
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
+
 
 // connect DB
 const connectDB = require('./db/connect')
@@ -36,6 +40,10 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
+})
+app.use('api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use('/api/v1/auth', authRouter)
